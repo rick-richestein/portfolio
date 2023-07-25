a88c310497b32f88e350711e965d93ef566b1042
utsav garg <utsgarg.ug@gmail.com>
2023-07-25 13:23:08 +0530

    experinece section imporved

--- app.js
+++ app.js
@@ -8,18 +8,11 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
-const defaultRoutes = require('./routes/default'); 
 app.use(bodyParser.urlencoded({extended:false}));
+
+const defaultRoutes = require('./routes/default');
+
 app.use(express.static(path.join(__dirname,'public')))
-
-/* app.use('/addProduct',(req,res,next)=>{
-    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">ADD PRODUCT</button></form>');
-});
-
-app.use('/product',(req,res,next)=>{   
-    console.log(req.body);
-    res.redirect("/");
-}); */
 
 app.use(defaultRoutes);
 
@@ -27,4 +20,4 @@ app.use((req,res,next)=>{
     res.status(404).send('<h1>404 Page</h1>');
     
 })
-app.listen(3000);
+app.listen(3001);
